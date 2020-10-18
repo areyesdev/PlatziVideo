5e16a691e856f6c87029e06a1b27f7d9a538a71a
Andres Reyes <ar97yf@gmail.com>
2020-10-17 19:55:19 -0500

    variables en scss e loader-images

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,10 +12,29 @@ const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+
+    <Categories title='Mi lista'>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Tendencias'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Originales de PlatziVideo'>
+      <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
